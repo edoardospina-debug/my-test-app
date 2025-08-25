b941b70c2baeab8b95d5bcc2c9093b8ae9b98e74
Edoardo Spina <edo@Edoardos-MacBook-Pro.local>
2025-08-26 00:23:37 +0100

    Ensure main.py is Vercel-compatible

--- src/my_test_app/main.py
+++ src/my_test_app/main.py
@@ -4,27 +4,31 @@ from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
 from dotenv import load_dotenv
 import os
+import uvicorn
 
 load_dotenv()
 
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://*.vercel.app", "http://localhost:3000"],  # Vercel and local React dev
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+CORSMiddleware,
+allow_origins=[""],
+allow_credentials=True,
+allow_methods=[""],
+allow_headers=["*"],
 )
 
 @app.get("/api/")
 async def root():
-    try:
-        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-        conn.close()
-        return {"message": "Hello, Vercel! Connected to PostgreSQL!"}
-    except Exception as e:
-        return JSONResponse(
-            status_code=500,
-            content={"message": f"Error connecting to PostgreSQL: {str(e)}"}
-        )
+	try:
+		conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+		conn.close()
+		return {"message": "Hello, Vercel! Connected to PostgreSQL!"}
+	except Exception as e:
+		return JSONResponse(
+			status_code=500,
+			content={"message": f"Error connecting to PostgreSQL: {str(e)}"}
+		)
+
+if __name__ == "__main__":
+	uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
