0c644c9ee3c7eb17f58f727f8b4813909969a30d
Edoardo Spina <edo@Edoardos-MacBook-Pro.local>
2025-08-26 01:52:05 +0100

    Ensure main.py is correct for FastAPI

--- src/my_test_app/main.py
+++ src/my_test_app/main.py
@@ -11,24 +11,24 @@ load_dotenv()
 app = FastAPI()
 
 app.add_middleware(
-CORSMiddleware,
-allow_origins=[""],
-allow_credentials=True,
-allow_methods=[""],
-allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/api/")
 async def root():
-	try:
-		conn = psycopg2.connect(os.getenv("DATABASE_URL"))
-		conn.close()
-		return {"message": "Hello, Vercel! Connected to PostgreSQL!"}
-	except Exception as e:
-		return JSONResponse(
-			status_code=500,
-			content={"message": f"Error connecting to PostgreSQL: {str(e)}"}
-		)
+    try:
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        conn.close()
+        return {"message": "Hello, Vercel! Connected to PostgreSQL!"}
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={"message": f"Error connecting to PostgreSQL: {str(e)}"}
+        )
 
 if __name__ == "__main__":
-	uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
